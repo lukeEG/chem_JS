ff326ef55083835e30ee99dd214329c56a8a7fff
spencerchu <spencerbchu@gmail.com>
2015-05-29 13:32:52 -0400

    Reogranized File Structure
    Moved chem_JS  up from test/examples level

--- jsPsych-4.1/tests&examples/save_data.php
+++
@@ -1,9 +0,0 @@
-<?php
-// the $_POST[] array will contain the passed in filename and data
-// the directory "data" is writable by the server (chmod 777)
-	//If you are having problems saving, make sure the data directory has permissions 777
-$filename = "data/".$_POST['filename'];
-$data = $_POST['filedata'];
-// write the file to disk
-file_put_contents($filename,$data);
-?>
